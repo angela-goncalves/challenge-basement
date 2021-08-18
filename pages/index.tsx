251fd3527722e6793dc6003a3e123ceb3195742e
angela-goncalves <angelakgo20@gmail.com>
2021-08-18 15:19:21 -0300

    add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import type {NextPage} from "next";
 import Image from "next/image";
 
-import logo from "../public/logo.svg";
+import imageheader from "../public/header.svg";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-full flex bg-black">
+    <div className="h-full bg-black">
+      <Navbar />
       <header className="m-auto text-white text-center">
-        <Image alt="Basement" src={logo} />
-        <h4>Lets get this party started</h4>
+        <div className="max-w-full px-4">
+          <Image alt="" className="w-full h-full" src={imageheader} />
+        </div>
       </header>
+      <main />
     </div>
   );
 };
