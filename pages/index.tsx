b46397c339243a0b9793887301d7b0f4aae50d0e
angela-goncalves <angelakgo20@gmail.com>
2021-08-20 19:57:40 -0300

    styles to placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from "next";
 import Image from "next/image";
 
+import Linkbar from "../components/Linkbar";
 import footer from "../public/footer.svg";
 import imageheader from "../public/header.svg";
 import data from "../product/mock.json";
@@ -10,12 +11,16 @@ import Navbar from "../components/Navbar";
 const Home: NextPage = () => {
   return (
     <div className="h-full bg-black">
+      <Linkbar />
       <Navbar />
       <header className="m-auto text-white text-center">
         <div className="max-w-full mx-4 md:mx-8">
           <Image alt="basement-supply" className="w-full h-full" src={imageheader} />
         </div>
       </header>
+      <div className="border-t-2 border-b-2 mt-2  mb-12 md:mt-14 md:mb-24 ">
+        <p className="text-xl  md:text-4xl">A man can´t have enough basement. swang -</p>
+      </div>
       <main className="grid md:grid-cols-3 mx-4 md:mx-7 space-x-4 md:space-x-8">
         {data.map((product) => {
           const {title, image, id, price} = product;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    textColor: {
+      textInput: "rgba(131, 229, 118, 1)",
+    },
+    placeholderColor: (theme) => ({
+      ...theme("colors"),
+      placeholder: "rgba(131, 229, 118, 1)",
+    }),
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      input: "rgba(118, 118, 128, 0.24)",
+      inputPlace: "rgba(255, 255, 255, 0.15)",
+    }),
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    // ...
+  ],
+};
