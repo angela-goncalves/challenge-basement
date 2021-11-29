4cd8c7dd7c64999ad040da0e7f92abcd99a9f902
Angela <angelakgo20@gmail.com>
2021-11-29 12:34:56 -0300

    add total to the cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,10 @@ import logomobile from "../public/logoMobile.svg";
 
 interface NavbarProps {
   counter: number;
+  setOpen: any;
 }
 
-const Navbar: FC<NavbarProps> = ({counter}) => {
+const Navbar: FC<NavbarProps> = ({counter, setOpen}) => {
   return (
     <div>
       <ul className="flex justify-between m-8 items-center">
@@ -18,8 +19,10 @@ const Navbar: FC<NavbarProps> = ({counter}) => {
         <li className="lg:hidden">
           <Image alt="" src={logomobile} />
         </li>
-        <li className="border-2 rounded-full px-5 md:px-8 py-3 ">
-          CART <span>({counter})</span>
+        <li>
+          <button className="border-2 rounded-full px-5 md:px-8 py-3" onClick={() => setOpen(true)}>
+            CART <span>({counter})</span>
+          </button>
         </li>
       </ul>
     </div>
--- components/Product.tsx
+++ components/Product.tsx
@@ -1,27 +1,10 @@
 import Image from "next/image";
-import React, {FC} from "react";
+import React from "react";
 
-interface ProductProps {
-  title: string;
-  image: string;
-  price: number;
-  id: number;
-  cart: any;
-  counter: number;
-  description: string;
-  addToCart: any;
-  setOpen: any;
-}
-const Product: FC<ProductProps> = ({
-  title,
-  image,
-  price,
-  description,
-  id,
-  setOpen,
-  addToCart,
-  counter,
-}) => {
+import {ProductType} from "../product/types";
+
+const Product = ({setOpen, product, addToCart, counter}: ProductType) => {
+  const {title, image, price, description, id} = product;
   const handleOnClick = () => {
     addToCart({title, image, price, description, id, counter});
     setOpen(true);
@@ -29,7 +12,7 @@ const Product: FC<ProductProps> = ({
 
   return (
     <div className="h-full">
-      <button className="h-full" onClick={() => handleOnClick()}>
+      <button className="h-full" onClick={handleOnClick}>
         <div className="h-full flex flex-col justify-between">
           <Image alt={title} height={550} src={image} width={450} />
           <div className="flex justify-between border-t-2 pt-2">
